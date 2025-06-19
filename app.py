5e21c9a5b0b648db5f39b3ea1ae382c34f28a3f6
SAHITHI <purnasahithi625@gmail.com>
2025-06-19 16:20:59 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/skills-projects")
+def skills_projects():
+    return render_template("skills-projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
